6198df4e38959de9d7ca2067b93c24aedc9ac2e5
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-05-12 20:37:38 -0300

    Lots of simplifications on models and the security/role system for teams

--- pytalaya/app/models.py
+++ pytalaya/app/models.py
@@ -9,9 +9,8 @@ class Team(models.Model):
     logo = models.ImageField(upload_to='logos')
     url = models.CharField(max_length=15, unique=True)
 
-    private = models.BooleanField()
-    private_password = models.CharField(max_length=100, blank=True)
-    require_auth = models.BooleanField()
+    admin_password = models.CharField(max_length=100, blank=True)
+    member_password = models.CharField(max_length=100, blank=True)
 
     def __unicode__(self):
         return self.name
@@ -25,31 +24,14 @@ class Status(models.Model):
     color = models.CharField(max_length=25)
 
 
-class User(models.Model):
-    '''A user.'''
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=255)
-    facebook = models.CharField(max_length=100, blank=True)
-    twitter = models.CharField(max_length=100, blank=True)
-
-
-ROLE_ADMIN, ROLE_WORKER, ROLE_OBSERVER = range(3)
-
-ROLES = (
-    (ROLE_ADMIN, u'Team Administrator'),
-    (ROLE_WORKER, u'Team Member'),
-    (ROLE_OBSERVER, u'Observer'),
-)
-
-
 class Member(models.Model):
     '''A team member.'''
-    user = models.ForeignKey(User)
     team = models.ForeignKey(Team)
-    role = models.IntegerField(choices=ROLES)
+    username = models.CharField(max_length=100)
+
     group_tags = models.TextField()
 
-    status = models.CharField(max_length=100)
+    status = models.ForeignKey(Status)
     status_extra = models.TextField()
     status_date = models.DateTimeField(auto_now=True)
 
