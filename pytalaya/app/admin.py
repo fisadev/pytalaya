f76eb15a300547d99a10bd5d38e24d8581c74caf
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-05-12 20:43:14 -0300

    Updated admin for the new models

--- pytalaya/app/admin.py
+++ pytalaya/app/admin.py
@@ -1,11 +1,10 @@
 #-*- coding:utf-8 -*-
 from django.contrib import admin
-from models import Team, Status, Member, User
+from models import Team, Status, Member
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'private', 'require_auth')
-    list_filter = ('private', 'require_auth')
+    list_display = ('name', 'url')
     search_fields = ('name', 'description')
 
 
@@ -16,18 +15,12 @@ class StatusAdmin(admin.ModelAdmin):
 
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('team', 'role', 'group_tags', 'user', 'status', 'status_date')
-    list_filter = ('team', 'role', 'group_tags', 'status', 'status_date')
-    search_fields = ('user__name', 'status', 'status_extra')
+    list_display = ('team', 'username', 'group_tags', 'status', 'status_date')
+    list_filter = ('team', 'group_tags', 'status', 'status_date')
+    search_fields = ('username', 'status', 'status_extra')
     date_hierarchy = 'status_date'
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name', )
 
 
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Member, MemberAdmin)
-admin.site.register(User, UserAdmin)
