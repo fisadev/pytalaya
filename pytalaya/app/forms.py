ce0fec4f2879b1a95395b5597381b14a97daf9a4
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-05-20 00:48:07 -0300

    Added join team form

--- pytalaya/app/forms.py
+++ pytalaya/app/forms.py
@@ -1,4 +1,5 @@
 #-*- coding:utf-8 -*-
+from django import forms
 from django.forms import ModelForm
 
 from app.models import Team
@@ -7,3 +8,8 @@ from app.models import Team
 class NewTeamForm(ModelForm):
     class Meta:
         model = Team
+
+class JoinTeamForm(forms.Form):
+    team_name = forms.CharField()
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
