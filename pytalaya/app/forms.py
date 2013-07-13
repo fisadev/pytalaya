8e29aba255523483f608ed46a39d67be9fd2e31b
Joaquin Tita <carpincho@gmail.com>
2013-07-13 17:29:45 -0300

    Removed django 1.6 field __all__

--- pytalaya/app/forms.py
+++ pytalaya/app/forms.py
@@ -6,10 +6,8 @@ from app.models import Team
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = '__all__'
 
 
 class JoinForm(forms.Form):
     user_name = forms.CharField(max_length=100)
     team = forms.SlugField(max_length=255)
-
