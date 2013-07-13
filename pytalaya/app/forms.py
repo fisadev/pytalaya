7b88ef97383ea1e80b9cd3ec209a3e30e819ea92
arielrossanigo <arielrossanigo@gmail.com>
2013-07-13 17:55:21 -0300

    some fixes

--- pytalaya/app/forms.py
+++ pytalaya/app/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from app.models import Team
+from app.models import Team, Member
 
 
 class TeamForm(forms.ModelForm):
@@ -10,3 +10,18 @@ class TeamForm(forms.ModelForm):
 class JoinForm(forms.Form):
     user_name = forms.CharField(max_length=100)
     team = forms.SlugField(max_length=255)
+
+    def clean_user_name(self):
+        #user must not exists
+        user = self.cleaned_data['user_name']
+        if Member.objects.filter(username=user).exists():
+            raise forms.ValidationError('User already exists. Choose another.')
+        return user
+
+    def clean_team(self):
+        #team must exists
+        team = self.cleaned_data['team']
+        if not Team.objects.filter(slug=team).exists():
+            raise forms.ValidationError('Team must exists')
+        return team
+
