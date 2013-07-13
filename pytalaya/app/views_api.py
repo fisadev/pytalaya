c36d0fb9f75673b47c1049dae72b8b2675202a07
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-07-13 15:39:28 -0300

    Refactor json_response logic

--- pytalaya/app/views_api.py
+++ pytalaya/app/views_api.py
@@ -2,12 +2,16 @@ import json
 
 from django.http import HttpResponse
 
-from app.models import Team
+from app.models import Team, Member
+
+
+def json_response(data):
+    return HttpResponse(json.dumps(data),
+                        content_type="application/json")
 
 
 def members(request, team_slug):
     team = Team.objects.get(slug=team_slug)
     members = team.members.select_related('area')
 
-    return HttpResponse(json.dumps(members),
-                        content_type="application/json")
+    return json_response(members)
