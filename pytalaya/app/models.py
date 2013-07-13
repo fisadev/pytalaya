68d38aca5a559f6c94466eb0e287aa51e561c20e
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-07-13 14:05:47 -0300

    Team slug must be unique

--- pytalaya/app/models.py
+++ pytalaya/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Team(models.Model):
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     private = models.BooleanField()
     password = models.CharField(max_length=100)
