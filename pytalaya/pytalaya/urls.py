c77d79e816cb564097f529d5efd3998ef156f7b5
Francisco <francisco@francisco-PC.(none)>
2013-07-13 15:54:19 -0300

    add create url

--- pytalaya/pytalaya/urls.py
+++ pytalaya/pytalaya/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
-from pytalaya import views
+
 
 
 urlpatterns = patterns('',
     url(r'^t/(?P<slug>[\w-]+)/$', views.dashboard),
     url(r'^api/members/(?P<team_slug>[\w-]+)/$', 'app.views_api.members', name='api_members'),
     url(r'^api/status/(?P<member_id>\d+)/$', 'app.views_api.status', name='api_status'),
+    url(r'^t/(?P<slug>[\w-]+)/$', 'dashboard', name='dashboard'),
+    url(r'^create/$', 'create', name='create'),
 )
