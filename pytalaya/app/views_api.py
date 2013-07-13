9343a3a5e6867ae58dbbb87e8fcf8e9ef7969c2c
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-07-13 15:11:00 -0300

    Is request, not self -_-

--- pytalaya/app/views_api.py
+++ pytalaya/app/views_api.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from app.models import Team
 
 
-def members(self, team_slug):
+def members(request, team_slug):
     team = Team.objects.get(slug=team_slug)
     members = team.members.select_related('area')
 
