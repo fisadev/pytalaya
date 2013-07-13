255b83b874f0af4449aed30f2938afbf6e598a25
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-07-13 15:41:51 -0300

    Fixed urls, the app name is app, not pytalaya

--- pytalaya/pytalaya/urls.py
+++ pytalaya/pytalaya/urls.py
@@ -4,6 +4,6 @@ from pytalaya import views
 
 urlpatterns = patterns('',
     url(r'^t/(?P<slug>[\w-]+)/$', views.dashboard),
-    url(r'^api/members/(?P<team_slug>[\w-]+)/$', 'pytalaya.views_api.members', name='api_members'),
-    url(r'^api/status/(?P<member_id>\d+)/$', 'pytalaya.views_api.status', name='api_status'),
+    url(r'^api/members/(?P<team_slug>[\w-]+)/$', 'app.views_api.members', name='api_members'),
+    url(r'^api/status/(?P<member_id>\d+)/$', 'app.views_api.status', name='api_status'),
 )
